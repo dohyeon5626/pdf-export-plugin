0c66e964904b64c0f8f5a9d824c1d84c2325fd56
dohyeon5626 <kodohyeon71@gmail.com>
2026-04-20 20:47:15 +0900

    Initial commit: bare Figma plugin skeleton

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlInlineScriptPlugin = require('html-inline-script-webpack-plugin');
+
+module.exports = (env, argv) => ({
+  mode: argv.mode === 'production' ? 'production' : 'development',
+  devtool: argv.mode === 'production' ? false : 'inline-source-map',
+  entry: {
+    code: './src/code.ts',
+    ui: './src/ui.ts',
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ },
+    ],
+  },
+  resolve: { extensions: ['.tsx', '.ts', '.js'] },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/ui.html',
+      filename: 'ui.html',
+      chunks: ['ui'],
+      inject: 'body',
+      cache: false,
+    }),
+    new HtmlInlineScriptPlugin({
+      htmlMatchPattern: [/ui.html/],
+    }),
+  ],
+});
